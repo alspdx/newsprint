2773d7ba953eeb93a867a0451b9325bdcc68225c
Adam Smith and Holly Wilkalis <adam@>
2017-11-30 15:18:44 -0800

    add other elements

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,12 @@ $(document).ready(function() {
     fullWidth: true,
     indicators: true
   });
+
+  $('.collapsible').collapsible();
+
+  $('.fixed-action-btn').floatingActionButton({
+    direction: 'top', // Direction menu comes out
+    hoverEnabled: true, // Hover enabled
+    toolbarEnabled: false // Toolbar transition enabled
+  });
 });
