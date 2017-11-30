24bacd29807c5fd3bd7aef790c7e53576947008d
Adam Smith and Holly Wilkalis <adam@>
2017-11-30 12:02:09 -0800

    add cards with images

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('.sidenav').sidenav();
 
+  $('.materialboxed').materialbox();
+
   $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
